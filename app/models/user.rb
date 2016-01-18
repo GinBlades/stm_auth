5528079ba43494a93a0e7c799b0a61dbd7207483
Shaun Martin <webmaster@ginblades.com>
2016-01-18 00:31:25 -0600

    Getting started with devise and doorkeeper.

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable,
+    :confirmable, :lockable, :timeoutable # and :omniauthable
+  enum role: {
+    guest: 0,
+    member: 10,
+    manager: 20,
+    admin: 30
+  }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  use_doorkeeper
   root "pages#home"
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
