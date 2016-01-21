11b87e5680e90a95f3f9b55d89260cd6bb8f174c
Shaun Martin <webmaster@ginblades.com>
2016-01-21 08:28:00 -0600

    Rename cookie.

--- app/helpers/oauth_cookie_helper.rb
+++ app/helpers/oauth_cookie_helper.rb
@@ -5,10 +5,10 @@ module OauthCookieHelper
   end
 
   def set_user_cookie(user)
-    cookies[:so_auth] = { value: user.id, domain: env_domain }
+    cookies[:stm_strategy] = { value: user.id, domain: env_domain }
   end
 
   def remove_user_cookie
-    cookies.delete(:so_auth, domain: env_domain)
+    cookies.delete(:stm_strategy, domain: env_domain)
   end
 end
